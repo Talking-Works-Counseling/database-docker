f93292f1877a7ae76065b1146b25fc383c0995d2
rohit2thinkitive <rohit.vishwakarma@thinkitive.com>
2024-08-27 12:44:45 +0530

    status

--- init.sql
+++ init.sql
@@ -1,5 +1,2 @@
 SELECT 'CREATE DATABASE talking_works_dev'
 WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'talking_works_dev')
-
-SELECT 'CREATE DATABASE keycloak'
-WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'keycloak')

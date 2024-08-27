79e73a9e1f4c330008ea7c6d2755e1a8eb4f9319
rohit2thinkitive <rohit.vishwakarma@thinkitive.com>
2024-08-27 12:30:32 +0530

    added database for keycloak also

--- init.sql
+++ init.sql
@@ -1,2 +1,5 @@
 SELECT 'CREATE DATABASE talking_works_dev'
 WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'talking_works_dev')
+
+SELECT 'CREATE DATABASE keycloak'
+WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'keycloak')

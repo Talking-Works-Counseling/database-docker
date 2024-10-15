ca7aae9b0d83e87bfc0386f27ea11dd46ac90913
rohit2thinkitive <rohit.vishwakarma@thinkitive.com>
2024-10-15 12:43:31 +0530

    added keycloak database init sql

--- init.sql
+++ init.sql
@@ -5,5 +5,10 @@ BEGIN
    ) THEN
       PERFORM dblink_exec('dbname=postgres', 'CREATE DATABASE talking_works_qa');
    END IF;
+
+      -- Check if 'keycloak' database exists, if not, create it
+   IF NOT EXISTS (SELECT FROM pg_database WHERE datname = 'keycloak') THEN
+         PERFORM dblink_exec('dbname=postgres', 'CREATE DATABASE keycloak');
+   END IF;
 END
 $$;
